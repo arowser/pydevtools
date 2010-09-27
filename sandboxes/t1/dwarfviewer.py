ad8c060e8f198cc1706548021b2f89c654074985
sebastien.colleur <sebastien.colleur@17db7e9f-cca9-650e-e268-553f2f50f789>
2010-09-27 17:13:54 +0000

    Creates branch for #1

---
+++ sandboxes/t1/dwarfviewer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from sys import argv
+
+from devtools.dwarf import DWARF
+from devtools.dwarf.viewer import Viewer
+
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        file_path = "./test/test"
+    else:
+        file_path = argv[1]
+    dwarf = DWARF(file_path)
+    Viewer(dwarf)
