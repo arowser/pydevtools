a16a2adbc848bcef2401bd529b1513ab0635bd6e
sebastien.colleur <sebastien.colleur@17db7e9f-cca9-650e-e268-553f2f50f789>
2010-10-01 09:20:01 +0000

    Terminates branch for #1

--- sandboxes/t1/devtools/elf/exception.py
+++
@@ -1,7 +0,0 @@
-"""
-Copyright (c) 2010, Cambridge Silicon Radio Ltd.
-Written by Emilio Monti <emilmont@gmail.com>
-"""
-class ParseError(Exception):
-    pass
-    
--- sandboxes/t1/test/a/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_A
-#define TEST_A
-
-void dummy_a(void);
-
-#endif
